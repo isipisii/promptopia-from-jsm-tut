4b7f99c13953549c6e810972cf436d94c8f559d6
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-05-11 22:00:43 +0800

    added the onclick and like counter

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,15 +1,26 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+const PromptCard = ({
+  post,
+  handleTagClick,
+  handleEdit,
+  handleDelete,
+  addLike,
+}) => {
   const [copied, setCopied] = useState("");
   const { data: session } = useSession();
+  // const [likeCount, setLikeCount] = useState(post.like);
   const pathName = usePathname();
   const router = useRouter();
+
+  // useEffect(() => {
+  //   setLikeCount(post.like);
+  // }, [post.like]);
 
   // copy to clipboard
   function handleCopy() {
@@ -21,33 +32,38 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   }
 
   // to nvaigate the user to the profile page whether it is their own or other users
-  function handleProfileClick(){
+  function handleProfileClick() {
     // if the user clicked is his own profile then it will be routed to his own profile
-   post.creator._id === session?.user?.id ? router.push("/profile") : 
-   router.push(`/profile/${post.creator._id}?name=${post.creator.username}`)  //  else it will be routed to the other user's profile
-  
-  } 
-  
+    post.creator._id === session?.user?.id
+      ? router.push("/profile")
+      : router.push(
+          `/profile/${post.creator._id}?name=${post.creator.username}`
+        ); //  else it will be routed to the other user's profile
+  }
+
   return (
     <div className="prompt_card h-auto">
       <div className="flex justify-between gap-5 items-start">
-          <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer" onClick={handleProfileClick}>
-            <Image
-              src={post.creator.image}
-              alt="user_image"
-              width={40}
-              height={40}
-              className="rounded-full object-contain"
-            />
-            <div className="flex flex-col" >
-              <h3 className="font-satoshi font-semibold text-gray-900">
-                {post.creator.username}
-              </h3>
-              <p className="font-inter text-sm text-gray-500">
-                {post.creator.email}
-              </p>
-            </div>
+        <div
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
+        >
+          <Image
+            src={post.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
           </div>
+        </div>
         <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={
@@ -68,14 +84,22 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         #{post.tag}
       </p>
-      
+
+      <button onClick={() => addLike(post)}>Like {post.like}</button>
+
       {/* will only render whenever it is in profile path and the session id is the same with creator id */}
       {session?.user?.id === post.creator._id && pathName === "/profile" && (
         <div className="mt-5 flex-center gap-4 border-t  border-gray-100 pt-3">
-          <p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
             Edit
           </p>
-          <p className="font-inter text-sm orange_gradient cursor-pointer" onClick={handleDelete}>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
             Delete
           </p>
         </div>
