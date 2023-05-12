22aaedf27fca63c02c9f6e96aaf1fbcdfe16f68d
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-05-12 21:16:45 +0800

    added dislike

--- app/api/like-prompt/[id]/route.js
+++ app/api/like-prompt/[id]/route.js
@@ -3,15 +3,30 @@ import { connectToDb } from "@/utils/database";
 
 export const POST = async (request, { params }) => {
   const { id } = params;
-  try {
+  const { userId } = request.body;
+
+ try {
     await connectToDb();
+    // will get the specific prompt post
     const promptPost = await Prompt.findById(id);
 
     if (!promptPost) {
       return new Response("Prompt not found", { status: 404 });
     }
 
-    promptPost.like += 1;
+    // accessing the likedBy property of the prompt post by destructring
+    const { likedBy } = promptPost;
+
+    // if the user includes in likedBy array the user will be able to dislike the post
+    if (likedBy.includes(userId)) {
+      promptPost.like -= 1;
+      promptPost.likedBy = likedBy.filter((userId) => userId !== userId);
+    } else {
+      // if the user does not include in likedBy array the user will be able to like the post and push it inside the likedBy array
+      promptPost.like += 1;
+      promptPost.likedBy.push(userId);
+    }
+
     await promptPost.save();
 
     return new Response(JSON.stringify(promptPost), { status: 200 });
